05f31289304d9af86c4152d2ed752f6e9af2225b
DH572 <115117969+DH572@users.noreply.github.com>
2022-11-04 01:33:23 -0700

    Added main menu and button function

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene("InGame");
+    }
+
+    public void quit()
+    {
+        Application.Quit();
+    }
+}
